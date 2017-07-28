324d634badeb8b5f22db06164095178041566f3b
Lim H <limdauto@gmail.com>
2017-07-28 15:55:17 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ test_requirements = [
 
 setup(
     name='drf_openapi',
-    version='0.4.0',
+    version='0.5.0',
     description="Utilities to generate OpenAPI-compatible schema from API made with Django Rest Framework",
     long_description=readme + '\n\n' + history,
     author="Lim H.",
